7641ccc8eb9895f2dc03f84ab6324e262893ba07
conormcharg <conormcharg@live.co.uk>
2021-02-25 14:26:56 +0000

    First Commit

---
+++ handler.py
@@ -0,0 +1,15 @@
+# handler.py
+
+import numpy as np
+import boto3
+
+
+def main(event, context):
+    a = np.arange(15).reshape(3, 5)
+
+    print("Your numpy array:")
+    print(a)
+
+
+if __name__ == "__main__":
+    main('', '')
